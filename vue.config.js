d39bb9bfd9749e058b2426a36c27b16c01fa386a
alantea <alan094@gmail.com>
2021-06-27 21:18:44 +0800

    Update page title

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,13 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/battlecat-tw-schedule/'
     : '/',
-    productionSourceMap: false
+  productionSourceMap: false,
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'Battle cat schedule generator'
+      return args
+    })
+  }
 }
